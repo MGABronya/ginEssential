e35bb79666c16b92952c9b1d61f6e1ad4dd416de
MGAronya <20jzhang@stu.edu.cn>
2022-07-23 16:49:59 +0800

    解决跨域问题

---
+++ test/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "regexp"
+
+func VerifyEmailFormat(email string) bool {
+	//pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*` //匹配电子邮箱
+	pattern := `^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`
+
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+} //此处为用于验证邮箱格式是否正确的工具函数
+
+func main() {
+	print(VerifyEmailFormat("2078048816@qq.com"))
+}
